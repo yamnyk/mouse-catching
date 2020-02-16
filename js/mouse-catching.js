364a2b5e9874b05b6459306bbd4f4a44fb424f23
yamnyk <notAunicorN2018>
2020-02-17 01:05:02 +0400

    add renderGameField()

--- js/mouse-catching.js
+++ js/mouse-catching.js
@@ -8,12 +8,12 @@ class MouseCatchingGame {
     x: null,
     y: null,
   };
-  controlPanel = {
+  controlPanel = Object.freeze({
     wrapper: document.createElement('div'),
     timeAndScore: document.createElement('span'),
     lvl: document.createElement('span'),
-    msg: document.createElement('span'),
-  };
+    // msg: document.createElement('span'),
+  });
   lvl = 1;
   time = 10;
   
@@ -21,8 +21,7 @@ class MouseCatchingGame {
   constructor(containerSelector) {
     const {controlPanel, lvl, time} = this;
     this.gameContainer = document.querySelector(containerSelector);
-    controlPanel.lvl.textContent = lvl;
-    controlPanel.timeAndScore.textContent = time;
+    
     const _STATUSES = Object.freeze({
       CREATED: 'CREATED',
       INITIALIZED: 'INITIALIZED',
@@ -51,8 +50,40 @@ class MouseCatchingGame {
   }
   
   init() {
+    this.renderGameField();
+    
     this.setState(this.getStateList().INITIALIZED);
     return this;
+  }
+  
+  renderGameField() {
+    const {controlPanel, lvl, time, circle, gameContainer} = this;
+  
+    controlPanel.lvl.textContent = lvl;
+    controlPanel.timeAndScore.textContent = time;
+    
+    circle.el.classList.add('red-circle');
+    controlPanel.wrapper.classList.add('game-table');
+    controlPanel.timeAndScore.classList.add('game-table__time');
+    controlPanel.lvl.classList.add('game-table__level');
+    
+    controlPanel.wrapper.append(
+      controlPanel.timeAndScore,
+      controlPanel.lvl
+    );
+  
+    const circleSize = this.lvl * 15;
+    circle.el.style.width = `${circleSize}px`;
+    circle.el.style.height = `${circleSize}px`;
+    circle.el.style.transform = `translate(${gameContainer.offsetWidth / 2 - circleSize}px, ${gameContainer.offsetHeight / 2 - circleSize}px)`;
+  
+    document.body.oncontextmenu = () => false;
+  
+    gameContainer.append(
+      controlPanel.wrapper,
+      circle.el
+    )
+    
   }
 }
 
